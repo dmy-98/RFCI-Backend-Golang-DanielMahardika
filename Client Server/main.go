b545b8ba1d62b899b044406515e753812c50eb29
Daniel Mahardika <daniel.mahardikay@gmail.com>
2020-12-15 11:23:00 +0700

    fix log

--- Client Server/main.go
+++ Client Server/main.go
@@ -4,9 +4,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
-	"fmt"
+	"log"
+	"encoding/json"
 	"time"
-	"io"
 )
 
 type BlogInput struct{
@@ -20,32 +20,24 @@ type BlogInput struct{
 
 
 func main() {
-	r:= gin.New()
-	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-
-	// your custom format
-	return fmt.Sprintf("[%s] \"%s %s %d\n",
-			param.TimeStamp.Format(time.RFC1123),
-			param.Method,
-			param.Path,
-			param.StatusCode,
-		)
-	}))
-	r.Use(gin.Recovery())
-
-	// r := gin.Default()
+	r := gin.Default()
 
 	r.POST("/blogs", func(c *gin.Context) {
-		f, _ := os.Create("author.log")
-		gin.DefaultWriter = io.MultiWriter(f)
 
-		// authorFile,err := os.OpenFile("author.log",os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		// titleFile,err := os.OpenFile("title.log",os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		// defer authorFile.Close()
-		// defer titleFile.Close()
+		authorFile,err := os.OpenFile("author.log",os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		titleFile,err := os.OpenFile("title.log",os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		commentsFile,err := os.OpenFile("comments.log",os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalln("Failed to open errors log file")
+		}
 
-		// loggerAuthor := log.New(authorFile, "", log.LstdFlags)
-		// loggerTitle := log.New(titleFile, "", log.LstdFlags)
+		defer authorFile.Close()
+		defer titleFile.Close()
+		defer commentsFile.Close()
+
+		loggerAuthor := log.New(authorFile, "", 0)
+		loggerTitle := log.New(titleFile, "", 0)
+		loggerComments := log.New(commentsFile, "", 0)
 		
 		var dataInput BlogInput
 
@@ -53,8 +45,24 @@ func main() {
 			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
 			return
 		}
-		// loggerAuthor.Printf("{\"author\": \"%s\"}",dataInput.Author)
-		// loggerTitle.Printf("{\"title\": \"%s\"}",dataInput.Title)
+		author,_:=json.Marshal(dataInput.Author)
+		title,_:=json.Marshal(dataInput.Title)
+		comments,_:=json.Marshal(dataInput.Comments)
+
+		loggerAuthor.Printf("[%s] %d {\"author\": %s}",
+			time.Now().Format(time.RFC3339),
+			http.StatusCreated,
+			string(author))
+
+		loggerTitle.Printf("[%s] %d {\"title\": %s}",
+			time.Now().Format(time.RFC3339),
+			http.StatusCreated,
+			string(title))
+
+		loggerComments.Printf("[%s] %d {\"comments\": %s}",
+			time.Now().Format(time.RFC3339),
+			http.StatusCreated,
+			string(comments))
 
 		c.JSON(http.StatusCreated, gin.H{"author": dataInput.Author,"title": dataInput.Title,"comments": dataInput.Comments})
 	})
